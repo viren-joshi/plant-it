e07e19a789cef68bf605408d0defad37f758a3fa
Viren Joshi <viren.joshi403@gmail.com>
2024-07-30 23:40:39 +0530

    update 2

--- Application/linux/flutter/generated_plugins.cmake
+++ Application/linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_selector_linux
   modal_progress_hud_nsn
   url_launcher_linux
 )
--- Application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import geolocator_apple
 import modal_progress_hud_nsn
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
